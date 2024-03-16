de8ab0adc6849d1a158dbb97f783e15f8116d2d1
Gabriel Rocha Salomão <62037704+GabrielRocha593@users.noreply.github.com>
2024-03-16 12:02:59 -0300

    Feito o primeiro desenho da tela principal.

---
+++ frontend/src/TechChallenge.Hackthon.Web/Controllers/ProcessamentosController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TechChallenge.Hackthon.Web.Models;
+
+namespace TechChallenge.Hackthon.Web.Controllers
+{
+    public class ProcessamentosController : Controller
+    {
+
+        public IActionResult Detalhes()
+        {
+            var viewModel = new ListaDeProcessamentosViewModel();            
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ frontend/src/TechChallenge.Hackthon.Web/Models/ListaDeProcessamentosViewModel.cs
@@ -0,0 +1,30 @@
+﻿namespace TechChallenge.Hackthon.Web.Models
+{
+    public class ListaDeProcessamentosViewModel
+    {
+        public List<Processamentos> Processamentos = new List<Processamentos>();
+
+        public ListaDeProcessamentosViewModel()
+        {
+            Processamentos.Add(new Processamentos());
+            Processamentos.Add(new Processamentos());
+            Processamentos.Add(new Processamentos());
+        }
+    }
+
+    public class Processamentos
+    {
+        public string Id { get; set; }
+        public string ArquivoZIP { get; set; }
+        public int StatusProcessamento { get; set; }
+
+        public Processamentos()
+        {
+            Id = Guid.NewGuid().ToString();
+            ArquivoZIP = "";
+            StatusProcessamento = 1;
+        }
+    }
+
+
+}
